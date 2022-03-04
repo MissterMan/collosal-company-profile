5d779717de2893c40f725dfc6f15984a01bac134
MissterMan <putrayogi4545@gmail.com>
2022-03-04 23:53:15 +0700

    first commit, creating navbar, hero, features

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmsans: ['"DM Sans"', "sans-serif"],
+        "noto-sans-display": ['"Noto Sans Display"', "sans-serif"],
+      },
+      colors: {
+        "main-dark": "#0B0B22",
+        "custom-green": "#16FCD2",
+        primary: "#6016FC",
+      },
+    },
+  },
+  plugins: [],
+};
